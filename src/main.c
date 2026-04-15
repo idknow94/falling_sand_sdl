dd92fceaae899988c76ef2f50a6c762552e463f3
Alek Aleksanyan <alekaleksanyan@Mac.home>
2026-04-15 11:19:03 +0300

    checkerboard

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
-#include <stddef.h>
 #define SDL_MAIN_USE_CALLBACKS 1  
-
+#include "SDL3/SDL_events.h"
+#include "SDL3/SDL_pixels.h"
+#include "SDL3/SDL_rect.h"
+#include "SDL3/SDL_render.h"
+#include <stddef.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
@@ -42,16 +45,19 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    const double now = ((double)SDL_GetTicks()) / 1000.0;  
-    
-
-    float red = (float) (0.5 + 0.5 * SDL_sin(now));
-    float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  
+    SDL_SetRenderDrawColorFloat(renderer, 0.0, 0.0, 0.0, SDL_ALPHA_OPAQUE_FLOAT);  
     
     SDL_RenderClear(renderer);
-
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            if ((x + y) % 2 == 0) {
+                SDL_SetRenderDrawColorFloat(renderer, 1.0, 1.0, 1.0, SDL_ALPHA_OPAQUE_FLOAT);
+                SDL_RenderRect(renderer, &(SDL_FRect){x, y, 1.0, 1.0});
+            }
+        }
+    }
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;  
